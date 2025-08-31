af29a5713eb6e8eac4fe21069735c93a38cc886c
manacorp1-admin <m.mazumbu@outlook.de>
2025-08-31 02:30:53 +0200

    ASP.NET Core Projekt initialisiert

--- MEDVBiServ/Program.cs
+++ MEDVBiServ/Program.cs
@@ -1,4 +1,5 @@
 using MEDVBiServ.Components;
+using MudBlazor.Services;
 
 namespace MEDVBiServ
 {
@@ -11,6 +12,28 @@ namespace MEDVBiServ
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+
+            // Add MudBlazor services
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.BottomRight;
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 5000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = MudBlazor.Variant.Filled;
+            });
+
+
+
+
+
+
+
+
 
             var app = builder.Build();
 
