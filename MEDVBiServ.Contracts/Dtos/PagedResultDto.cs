da0de30291be8d07c7a5254fff67757b1527447d
Manasseé <m.mazumbu@outlook.de>
2025-12-16 11:48:27 +0100

    Klassenbibliothek Contracts wurden erstellt und paging, requests und sprache als enum für den API-Client wurde hinzugefügt

---
+++ MEDVBiServ.Contracts/Dtos/PagedResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDVBiServ.Contracts.Dtos
+{
+    public sealed class PagedResultDto<T>
+    {
+        public IReadOnlyList<T> Items { get; init; } = [];
+        public int Page { get; init; }
+        public int PageSize { get; init; }
+        public int TotalCount { get; init; }
+    }
+}
